449c2b1040c2d3b58af1930ec2fa72e0e847edd4
Manuel Duarte <carlosd99m@gmail.com>
2024-10-04 01:45:58 -0600

    wip: initial code

---
+++ README.md
@@ -0,0 +1,3 @@
+# comprobantes
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/comprobantes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.comprobantes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
